b4c4891fe1c30a88bc813aa4f5330b24e509bc80
Tim Cooke <tim@trcooke.co.uk>
2016-09-27 13:46:17 +0100

    Made the learned resources part of the running webserver rather than a shared def. Solves the problem of shared state between multiple servers run up via the Clojure programmatic API. Also added a very rough draft of a web page view of the application status.

--- src/ws_parrot/core.clj
+++ src/ws_parrot/core.clj
@@ -6,23 +6,26 @@
             [ring.adapter.jetty :refer :all]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(def saved-routes (atom '()))
-
-(defroutes app-routes
-  (POST "/parrotthis" [uri response] (swap! saved-routes conj {:uri uri :response response}))
-  (GET "/parrotthis" [] (generate-string (deref saved-routes)))
-  (ANY "/*" req (->> (deref saved-routes)
+(defn ws [port]
+  (let [savedroutes (atom '())
+        routes (defroutes app-routes
+                  (POST "/parrotthis" [uri method response] (swap! savedroutes conj {:uri uri :method (clojure.string/lower-case method) :response response}))
+                  (GET "/parrotthis" [] (generate-string (deref savedroutes)))
+                  (ANY "/req" req (println (:request-method req)))
+                  (ANY "/*" req (->> (deref savedroutes)
                      (filter #(= (:uri %) (:* (:params req))))
+                     (filter #(= (keyword (:method %)) (:request-method req)))
                      (first)
-                     (:response)))
-  (route/resources "/")
-  (route/not-found "Not Found"))
-
-(def app
-  (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
+                     (:response)
+                     (str)))
+                  (route/resources "/")
+                  (route/not-found "Not Found"))
+        app (wrap-defaults routes (assoc-in site-defaults [:security :anti-forgery] false))]
+    (run-jetty app {:port port :join? false})))
 
 (defn start-webserver [port]
-  (run-jetty app {:port port :join? false}))
+  (ws port))
 
 (defn -main [& args]
-  (run-jetty app {:port (read-string (first args))}))
+  (let [port (read-string (first args))]
+  (start-webserver port)))
--- test/ws_parrot/core_test.clj
+++ test/ws_parrot/core_test.clj
@@ -5,7 +5,17 @@
             [clj-http.client :as client]))
 
 (deftest test-app
-  (testing "http client stuff"
+  (testing "Save nothing, do a GET request, No response content."
+    (let [server (start-webserver 9000)]
+      (is (= (:body (client/get "http://localhost:9000/test")) ""))))
+
+  (testing "Save a GET response, do a GET request, response recieved."
     (let [server (start-webserver 9001)
-          postres (client/post "http://localhost:9001/parrotthis" {:form-params {"uri" "test" "response" "Test content"}})]
-      (is (= (:body (client/get "http://localhost:9001/test")) "Test content")))))
+          postres (client/post "http://localhost:9001/parrotthis" {:form-params {"uri" "test" "method" "GET" "response" "Test content 9001"}})]
+      (is (= (:body (client/get "http://localhost:9001/test")) "Test content 9001"))))
+
+  (testing "Save a POST response, do a GET request, No response content."
+    (let [server (start-webserver 9002)
+          postres (client/post "http://localhost:9002/parrotthis" {:form-params {"uri" "test" "method" "POST" "response" "Test content 9002"}})]
+      (is (= (:body (client/get "http://localhost:9002/test")) ""))))
+  )
